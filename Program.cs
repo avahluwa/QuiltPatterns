98255572318a62694cef51863630e7ee14ea9588
Avi Ahluwalia <avahluwa@microsoft.com>
2021-09-07 21:39:05 -0400

    Remove files from folder

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace QuiltPattern
     {
         static void Main(string[] args)
         {
+            
+
+
             Console.WriteLine("Greetings and welcome to the Quilt Pattern Logger!!");
+            Console.WriteLine("\nChoose a number to change the text color.");
+            Console.WriteLine("\n1) Red\n2) Green\n3) Blue");
         }
     }
 }
